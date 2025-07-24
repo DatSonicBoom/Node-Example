77a65a3e9138cf7c93b00ae15c29d8e3865b9866
Joshua / Catherine Moylan <35865600+DatSonicBoom@users.noreply.github.com>
2025-07-24 15:31:36 +1000

    Add Express example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log(1+1);
+
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.status(200).json({ "Author": "Catherine" });
+});
+
+app.listen(3000);
